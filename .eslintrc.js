8cb0f3845db78a1b56d1638d07be8025bc10d0dc
Arooj Fatima <arooj.fatima@arbisoft.com>
2023-11-15 06:40:16 +0500

    Initial Configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "eslint:all",
+  "rules": {
+    "no-unused-vars": "error",
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "env": {
+    "es6": true
+  },
+}
